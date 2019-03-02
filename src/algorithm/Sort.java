f36a71851b90d3780164e978341fa797264d4721
Adeeb Hasan <adeebplabon@gmail.com>
2019-03-02 16:44:32 -0500

    Test Sort

--- src/algorithm/Sort.java
+++ src/algorithm/Sort.java
@@ -34,9 +34,20 @@ public class Sort {
     public int[] insertionSort(int [] array){
         final long startTime = System.currentTimeMillis();
         int [] list = array;
-        //implement here
+        for (int i = 1; i < array.length; i++)
+        {
+            int key = array[i];
+            int j = i-1;
+            while (j >= 0 && array[j] > key)
+            {
+                array[j+1] = array[j];
+                j = j-1;
+            }
+            array[j+1] = key;
+        }
 
 
+    //implement here
 
         final long endTime = System.currentTimeMillis();
         final long executionTime = endTime - startTime;
@@ -47,12 +58,65 @@ public class Sort {
     public int[] bubbleSort(int [] array){
         int [] list = array;
         //implement here
+        int i, j;
+        for (i = 0; i < n-1; i++)
 
-        
-        
+            // Last i elements are already in place
+            for (j = 0; j < n-i-1; j++)
+                if (array[j] > array[j+1]) {
+                   int temp = array[j];
+                   array[j]= array[j+1];
+                   array[j+1] = temp;
+                }
+
         return list;
     }
-    
+    void merge(int arr[], int l, int m, int r)
+    {
+        int i, j, k;
+        int n1 = m - l + 1;
+        int n2 =  r - m;
+
+
+        int L[n1], R[n2];
+
+        for (i = 0; i < n1; i++)
+            L[i] = arr[l + i];
+        for (j = 0; j < n2; j++)
+            R[j] = arr[m + 1+ j];
+
+        i = 0;
+        j = 0;
+        k = l;
+        while (i < n1 && j < n2)
+        {
+            if (L[i] <= R[j])
+            {
+                arr[k] = L[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
 
     public int [] mergeSort(int [] array){
         int [] list = array;
